397c995ec7791976f44888a7a48457d8b7df2802
gaston <gastonCss_16@hotmail.com>
2018-11-10 19:52:56 -0300

    Casi terminado

--- TP-Funcional-EjercicioGOJam2015-A.hs
+++ TP-Funcional-EjercicioGOJam2015-A.hs
@@ -1,4 +1,5 @@
 import Prelude
+import Data.Char
 
 save :: (Show a)=> a -> IO() 
 save x = writeFile "datosOut.txt" (show x)
@@ -25,8 +26,40 @@ concatenarHead c [] = [[c]]
 concatenarHead c (cs:css) = (c:cs):css
 
 
---DeterminarCantidadDeAmigosNecesarios: [Char]-> Int
---DeterminarCantidadDeAmigosNecesarios
+
+cantDeAmigosPorTest:: [[Char]]-> [(Int,Int)]
+cantDeAmigosPorTest css  = (separarEnTuplasPorTest  css 1)
+
+separarEnTuplasPorTest:: [[Char]]-> Int-> [(Int,Int)]
+separarEnTuplasPorTest [] n = []
+separarEnTuplasPorTest (cs:css) n = (n, cantidadDeAmigosNecesarios cs): (separarEnTuplasPorTest css (n+1)) 
+
+
+
+
+cantidadDeAmigosNecesarios:: [Char]->Int
+cantidadDeAmigosNecesarios cs = agregarAmigoSiEsNecesario cs  0 -- if (esNecesarioAgregarAmigos cs) then (agregarAmigo cs)
+
+agregarAmigoSiEsNecesario::[Char]->Int ->Int
+agregarAmigoSiEsNecesario cs n = if (esNecesarioAgregarAmigos cs) then agregarAmigoSiEsNecesario (agregarAmigo cs 0 ) (n+1) else n 
+
+agregarAmigo::[Char]-> Int ->[Char]
+--Agrega un amigo de SI =0 siempre 
+agregarAmigo (c:cs) n = if ( n == 0 ) then c:(intToDigit (digitToInt (head cs) +1): (tail cs)) else c:cs 
+
+
+esNecesarioAgregarAmigos:: [Char]-> Bool
+esNecesarioAgregarAmigos cs =  h (tail cs) (head cs)
+                               where h cs c = (digitToInt c) > (contarCuantaGenteSeLevanta cs 0 0)
+
+
+contarCuantaGenteSeLevanta::[Char]-> Int -> Int -> Int -- z son la cantidad de personas con nivel de timidez -1
+contarCuantaGenteSeLevanta [] n z = z
+contarCuantaGenteSeLevanta (c:cs) n z =  h z n (digitToInt c) (contarCuantaGenteSeLevanta cs (n+1) ((digitToInt c) + z) ) (contarCuantaGenteSeLevanta cs (n+1)  z )
+                                          where h z n nc r1 r2= if (z >= n) then r1 else r2
+
+
+                                     
 
 
 
@@ -35,8 +68,9 @@ gestionarCasos f = do
                      x <- readFile f
                      xs <- fmap sacarEntersAndEspaciosFold (return x)
                      xsSep <- fmap separarPorCasosFold (return xs)
-                     save xsSep 
-                     print xsSep 
+                     xsRes <- fmap cantDeAmigosPorTest (return xsSep) 
+                     save xsRes 
+                     print xsRes 
 
 main = do 
        x <- readFile "sarasa.txt"
